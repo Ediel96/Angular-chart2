86bf60d821420be0d3d05305abc89d997588a485
DarkEdiel96 <39193308+Ediel96@users.noreply.github.com>
2020-05-18 23:05:20 -0500

    Agregando chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GraficComponent } from './grafic/grafic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraficComponent
   ],
   imports: [
     BrowserModule
